d80bf368b8dcb0a5ef3bdfb875e9d0b6deeb052c
Elisa Kokkonen <kokkonen.elisa@gmail.com>
2018-10-28 00:15:07 +0300

    Info content update

--- components/Info.vue
+++ components/Info.vue
@@ -1,8 +1,8 @@
 <template>
   <section id="info" class="info">
-    <p><img class="profile-image" src="assets/mie.jpg" alt=""></p>
+    <p><img class="profile-image" src="/mie.jpg" alt=""></p>
     <h1>Kuka Elisa?</h1>
-    <p>Työskentelen kymmenettä vuotta web-kehityksen parissa. Työni on teknistä ja järjestelmällistä, mutta samalla luovaa ongelman ratkaisua. Tästä kombinaatiosta pidän eniten työssäni. Tekniikan vastapainoksi käytän vapaa-aikaani vanhuksien avustamiseen ja itseni kehittämiseen erilaisilla sosiaalialan kursseilla. Harrastan myös luonnossa liikkumista sekä kaikenlaista käsillä puuhastelua.</p>
+    <p>Työskentelen kymmenettä vuotta web-kehityksen parissa. Työni on teknistä ja järjestelmällistä, mutta samalla luovaa ongelman ratkaisua ja kommukointia eri osapuolten välillä. Tästä kombinaatiosta pidän eniten työssäni. Tekniikan vastapainoksi käytän vapaa-aikaani vanhuksien avustamiseen ja itseni kehittämiseen erilaisilla sosiaalialan kursseilla. Harrastan myös luonnossa liikkumista sekä kaikenlaista käsillä puuhastelua.</p>
     <p>"Voi oppia mitä vain, kunhan omistaa intohimon, motivaation ja hyvän opettajan"</p>
   </section>
 </template>
@@ -11,6 +11,8 @@
 </script>
 
 <style lang="scss">
+@import 'assets/scss/colors.scss';
+@import 'assets/scss/general.scss';
 .info {
   text-align: center;
 
@@ -20,12 +22,18 @@
 
   p {
     font-size: 1.5rem;
-    margin: 0 auto;
+    margin: 1.5rem auto;
     max-width: 70%;
 
     @media only screen and (max-width: 768px) {
       max-width: 100%;
     }
+  }
+
+  .profile-image {
+    @include border-radius(100px);
+    height: auto;
+    width: 200px;
   }
 }
 </style>
